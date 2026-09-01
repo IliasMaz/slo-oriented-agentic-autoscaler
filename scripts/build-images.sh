1d8637387d279cd682fd2e3eef46854c7049a56e
IliasMaz <162236667+IliasMaz@users.noreply.github.com>
2026-09-01 22:37:42 +0300

    fix: build images with shared dependencies

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-docker build -t demo-app:latest ./app
-docker build -t agent-autoscaler:latest ./autoscaler
+docker build -f app/Dockerfile -t demo-app:latest .
+docker build -f autoscaler/Dockerfile -t agent-autoscaler:latest .
 
 kind load docker-image demo-app:latest --name thesis-autoscaling
 kind load docker-image agent-autoscaler:latest --name thesis-autoscaling
